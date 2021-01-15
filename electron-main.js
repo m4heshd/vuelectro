1b92af2af48f986070fb583066a71b4939f68981
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2021-01-16 05:02:37 +0530

    Add electron support and tests

---
+++ electron-main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile(path.resolve(".", "dist", "index.html")).then(() => {
+        win.show();
+        win.webContents.openDevTools();
+    });
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const path = require('path');
+
 createApp(App).use(store).use(router).mount('#app')
+
+console.log(path.resolve(__dirname));
