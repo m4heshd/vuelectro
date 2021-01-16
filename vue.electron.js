ba4636a686cad57dbf0e25505c1b0b1b60aef8dc
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2021-01-16 14:54:44 +0530

    Update vue.electron.js to support multiple functions

--- vue.electron.js
+++ vue.electron.js
@@ -1,19 +1,31 @@
 const {spawn} = require('child_process');
 const path = require('path');
 const vueService = require('@vue/cli-service');
-const {info,error} = require('@vue/cli-shared-utils');
+const {info, error} = require('@vue/cli-shared-utils');
 
 const service = new vueService(process.cwd());
 
-service.init("development");
-service.run('serve').then(({server, url}) => {
-    info('Launching Electron...');
+let args = process.argv.slice(2);
 
-    let electron = spawn(path.join('node_modules', '.bin', process.platform === 'win32' ? 'electron.cmd' : 'electron'), ['electron-main.js'], {stdio: 'inherit'});
+switch (args[0]) {
+    case 'serve':
+        serveDev();
+        break;
+    default:
+        console.error('Invalid argument');
+}
 
-    electron.on('exit', function (code) {
-        process.exit(0);
+function serveDev() {
+    service.init("development");
+    service.run('serve').then(({server, url}) => {
+        info('Launching Electron...');
+
+        let electron = spawn(path.join('node_modules', '.bin', process.platform === 'win32' ? 'electron.cmd' : 'electron'), ['electron-main.js'], {stdio: 'inherit'});
+
+        electron.on('exit', function (code) {
+            process.exit(0);
+        });
+    }).catch((err) => {
+        error(err.stack);
     });
-}).catch((err) => {
-    error(err.stack);
-});
+}
