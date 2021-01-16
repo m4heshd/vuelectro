33829394fae1d12e418d1b4ba3760deebc9dd50f
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2021-01-16 10:08:16 +0530

    Add Vue dev tools support and change BrowserWindow resources

--- electron-main.js
+++ electron-main.js
@@ -1,32 +1,51 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+let installExtension = require('electron-devtools-installer').default;
 
 function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        show: false,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         }
     });
 
-    win.loadFile(path.resolve(".", "dist", "index.html")).then(() => {
+    // win.loadFile(path.resolve(".", "dist", "index.html")).then(() => {
+    //     win.show();
+    //     // win.webContents.openDevTools();
+    // });
+    win.loadURL("http://localhost:8080/").then(() => {
         win.show();
         win.webContents.openDevTools();
     });
 }
 
-app.whenReady().then(createWindow)
+app.on('ready', async () => {
+
+    // Install Vue Devtools
+    try {
+        await installExtension({
+            id: 'ljjemllljcmogpfapbkkighbhhppjdbg', //Vue Devtools beta
+            electron: '>=1.2.1'
+        })
+    } catch (e) {
+        console.error('Vue Devtools failed to install:', e.toString())
+    }
+
+    createWindow();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        createWindow();
     }
-})
+});
